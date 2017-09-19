a4e63ad0cc39b6fcb5152aa9d327e0d0b76839c4
Gim YoungPhil <youngphil.gim@gmail.com>
2017-09-19 11:54:12 +0900

    add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <div class="container">
-    <h1>Hello App!</h1>
-    <p>
-      <!-- use router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
-      <router-link to="/">Home</router-link>
-      <router-link to="/hiragana">Go to Hiragana</router-link>
-      <router-link to="/pronounce">Go to Pronounce</router-link>
-    </p>
+  <div class="">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './app/Navbar.vue'
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+
+  components: { Navbar }
 }
 </script>
 
---
+++ src/app/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Hiragana</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link class="nav-item" active-class="active" to="/" tag="li" exact>
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link class="nav-item" active-class="active" to="/hiragana" tag="li">
+          <a class="nav-link">Hiragana</a>
+        </router-link>
+        <router-link class="nav-item" active-class="active" to="/pronounce" tag="li">
+          <a class="nav-link">Pronounce</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
