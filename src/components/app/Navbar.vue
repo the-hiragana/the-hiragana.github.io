f5e3365b02596e6a22671a0889e048b643cf8ec3
Gim YoungPhil <youngphil.gim@gmail.com>
2017-09-21 19:53:30 +0900

    fixed Navbar

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -8,15 +8,6 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <router-link class="nav-item" active-class="active" to="/" tag="li" exact>
-          <a class="nav-link">Home</a>
-        </router-link>
-        <router-link class="nav-item" active-class="active" to="/hiragana" tag="li">
-          <a class="nav-link">Hiragana</a>
-        </router-link>
-        <router-link class="nav-item" active-class="active" to="/pronounce" tag="li">
-          <a class="nav-link">Pronounce</a>
-        </router-link>
-        <router-link class="nav-item" active-class="active" to="/" tag="li" exact>
           <a class="nav-link">홈</a>
         </router-link>
         <router-link class="nav-item" active-class="active" to="/fiftycharacters" tag="li">
