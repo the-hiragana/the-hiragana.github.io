a1ecbcf646bee49a4a4137390b81307c9b47a150
Gim YoungPhil <youngphil.gim@gmail.com>
2017-09-27 16:38:31 +0900

    fixed pronunciationsOne Row

---
+++ src/components/pronunciationsOne/Row.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="my-5">
+    <button type="button" class="btn btn-outline-secondary mb-3">{{ row.row }}</button>
+    <div class="card-group">
+      <div class="card card-pron text-center mb-3"
+           v-for="(character, index) in row.characters"
+           v-bind:key="index"
+           v-bind:class="[!character.character ? 'd-none d-sm-block' : '', '']">
+        <div class="phonetic circle rounded-circle" v-if="character.phonetic">{{ character.phonetic }}</div>
+        <div class="card-img-top wf-notosansjapanese" v-if="character.character">
+          <h1 class="display-4 py-3 mb-0">{{ character.character }}</h1>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title mb-0">{{ character.word }}</h5>
+          <p class="card-text mb-0">{{ character.mean }}</p>
+        </div>
+      </div>
+    </div>
+    <p><span class="badge badge-success">TIP</span> {{ row.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'row',
+  ],
+}
+</script>
+
+<style>
+.card-pron {
+  border-color: #fff;
+}
+
+.card-pron .phonetic {
+  position: absolute;
+  right: 5px;
+  top: -15px;
+  width: 30px;
+  height: 30px;
+  background-color: #ff6969;
+}
+
+.card-pron .card-img-top {
+  background-color: #ffbbbb;
+}
+
+.card-pron .card-body {
+  background-color: #f6f6f6;
+}
+</style>
--- src/components/pronunciationsOne/RowList.vue
+++ src/components/pronunciationsOne/RowList.vue
@@ -2,57 +2,25 @@
   <div class="container">
     <div class="mt-5">
       <!-- <h2>{{ section.mean }}</h2> -->
-      <div class="my-5" v-for="row in section.rows" :key="row.row">
-        <button type="button" class="btn btn-outline-secondary mb-3">{{ row.row }}</button>
-        <div class="card-group">
-          <div class="card card-pron text-center mb-3"
-               v-for="(character, index) in row.characters"
-               v-bind:key="index"
-               v-bind:class="[!character.character ? 'd-none d-sm-block' : '', '']">
-            <div class="phonetic circle rounded-circle" v-if="character.phonetic">{{ character.phonetic }}</div>
-            <div class="card-img-top wf-notosansjapanese" v-if="character.character">
-              <h1 class="display-4 py-3 mb-0">{{ character.character }}</h1>
-            </div>
-            <div class="card-body">
-              <h5 class="card-title mb-0">{{ character.word }}</h5>
-              <p class="card-text mb-0">{{ character.mean }}</p>
-            </div>
-          </div>
-        </div>
-        <p><span class="badge badge-success">TIP</span> {{ row.description }}</p>
-      </div>
+      <Row v-for="row in section.rows" :key="row.row" :row="row"></Row>
     </div>
   </div>
 </template>
 
 <script>
+import Row from './Row.vue'
 
 export default {
   props: [
     'section',
   ],
+
+  components: {
+    Row,
+  },
 }
 </script>
 
 <style>
-.card-pron {
-  border-color: #fff;
-}
 
-.card-pron .phonetic {
-  position: absolute;
-  right: 5px;
-  top: -15px;
-  width: 30px;
-  height: 30px;
-  background-color: #ff6969;
-}
-
-.card-pron .card-img-top {
-  background-color: #ffbbbb;
-}
-
-.card-pron .card-body {
-  background-color: #f6f6f6;
-}
 </style>
