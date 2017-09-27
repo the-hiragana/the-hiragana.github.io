080da88a0819a46da37954e189a3f71b487e159a
Gim YoungPhil <youngphil.gim@gmail.com>
2017-09-27 17:41:23 +0900

    fixed historyMode

--- src/main.js
+++ src/main.js
@@ -22,7 +22,11 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 new Vue({
