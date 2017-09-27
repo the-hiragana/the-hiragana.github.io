ff32ceec3b63b7ab70fb953a1eab2527aff7b97e
Gim YoungPhil <youngphil.gim@gmail.com>
2017-09-27 16:38:10 +0900

    fixed hiraganaRow

--- src/components/hiragana/RowList.vue
+++ src/components/hiragana/RowList.vue
@@ -2,35 +2,26 @@
   <div class="container">
     <div class="mt-5">
       <div class="my-5" v-for="row in section.rows" :key="row.row">
-        <div class="card-group">
-          <div class="card card-hiragana text-center mb-3"
-               v-for="(character, index) in row"
-               v-bind:key="index"
-               v-bind:class="[!character.character ? 'd-none d-sm-block' : '', '']">
-            <div class="card-img-top wf-notosansjapanese" v-if="character.character">
-              <h1 class="display-4 py-3 mb-0">{{ character.character }}</h1>
-            </div>
-            <div class="card-body">
-              <h5 class="card-title mb-0">{{ character.word }}</h5>
-              <p class="card-text mb-0">{{ character.mean }}</p>
-            </div>
-          </div>
-        </div>
+        <Row :row="row"></Row>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Row from './Row.vue'
+
 export default {
   props: [
     'section',
   ],
+
+  components: {
+    Row,
+  },
 }
 </script>
 
 <style>
-.card-hiragana .card-img-top {
-  background-color: #ffeebb;
-}
+
 </style>
